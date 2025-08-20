adbe9eac03ab59820515cb365aa2c7ef94a88bc5
dhruvika <purohitdhruvika87@gmail.com>
2025-08-20 19:52:13 +0530

    20-08-2025

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './About.css'
 
-export default function About() {
+export default function About(props) {
+  const{name,bio,avatar_url}=props.user;
   return (
     <section className='about-section'>
-      <div className="content">
-        <h1>Dhruvika Purohit </h1>
-      <h4>Full Stack Developer</h4>
+      <div className="about">
+        <div className="content">
+        <h1>{name}</h1>
+      <h4>{bio}</h4>
       <p>Hi, I’m Dhruvika Purohit, a passionate Web Developer & Python Enthusiast. I love building responsive websites and scalable applications using HTML, CSS, JavaScript, React, Python, and SQL. Along with coding, I also have skills in Adobe Photoshop, Git, GitHub, and VS Code, which help me combine creativity with technology. My goal is to create impactful projects that deliver great user experiences.</p>
+      </div>
+      <div className="image">
+        <img src={avatar_url} alt="" />
+      </div>
       </div>
     </section>
   )
--- portfolio/src/screens/Home/Home.jsx
+++ portfolio/src/screens/Home/Home.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import About from '../../components/About/About'
+import axios from 'axios';
 
 export default function Home() {
+
+  const[githubData,setGithubData]=useState({});
+  useEffect(()=>{
+    fetchAPI().then((data) => setGithubData(data))
+  },[])
+  const fetchAPI=async()=>{
+    const res=await axios.get("https://api.github.com/users/PurohitDhruvika007");
+    return res.data;
+  }
   return (
     <div>
-      <About/>
+      <About user={githubData}/>
     </div>
   )
 }
