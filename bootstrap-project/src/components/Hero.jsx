d899c3e64d91d5f0a98f4620b9a22960f3a24195
dhruvika <purohitdhruvika87@gmail.com>
2025-09-07 00:13:20 +0530

    07-09-2025

--- bootstrap-project/src/components/Hero.jsx
+++ bootstrap-project/src/components/Hero.jsx
@@ -32,19 +32,19 @@ export default function Hero() {
                 </div>
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src="https://i.pinimg.com/736x/e4/dc/d8/e4dcd8a80475b99cef4d66dc8df94909.jpg" className="d-block w-100" style={{ height: 500 }} alt="..." />
+                        <img src="https://i.pinimg.com/736x/e4/dc/d8/e4dcd8a80475b99cef4d66dc8df94909.jpg" className="d-block w-100" style={{ height: 550 }} alt="..." />
                         <div className="carousel-caption d-none d-md-block">
 
                         </div>
                     </div>
                     <div className="carousel-item">
-                        <img src="https://i.pinimg.com/1200x/75/0e/1c/750e1cca56a0da94c466e4d75f189630.jpg" className="d-block w-100" style={{ height: 500 }} alt="..." />
+                        <img src="https://i.pinimg.com/1200x/56/83/ad/5683adca43e6cb9f5f68affa55cc6d88.jpg" className="d-block w-100" style={{ height: 550 }} alt="..." />
                         <div className="carousel-caption d-none d-md-block">
 
                         </div>
                     </div>
                     <div className="carousel-item">
-                        <img src="https://i.pinimg.com/736x/ec/6a/85/ec6a85f49f587f53071d9c03beb31c1a.jpg" className="d-block w-100" style={{ height: 500 }} alt="..." />
+                        <img src="https://i.pinimg.com/736x/ec/6a/85/ec6a85f49f587f53071d9c03beb31c1a.jpg" className="d-block w-100" style={{ height: 550 }} alt="..." />
                         <div className="carousel-caption d-none d-md-block">
                         </div>
                     </div>
