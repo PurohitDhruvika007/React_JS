c33fe68c8d6ca7993337badf9a0d47f35b08267d
dhruvika <purohitdhruvika87@gmail.com>
2025-10-10 10:49:49 +0530

    10-10-2025

---
+++ restaurant/src/main.jsx
@@ -0,0 +1,11 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+
+  <App />
+
+)
+//npm i axios,react-router,json-server,react-redux,@reduxjs/toolkit
