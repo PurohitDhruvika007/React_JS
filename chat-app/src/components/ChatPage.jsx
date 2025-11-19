05233c8b259afc8401525dedbd34ba949d268b99
dhruvika <purohitdhruvika87@gmail.com>
2025-11-19 21:21:02 +0530

    19-11-2025

--- chat-app/src/components/ChatPage.jsx
+++ chat-app/src/components/ChatPage.jsx
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router'
 import { useSelector, useDispatch } from 'react-redux';
-import { sendMessage } from '../slices/chatSlice';
+import { sendMessage, readMessage } from '../slices/chatSlice';
 import { getuser } from '../slices/userSlice';
 
 export default function ChatPage() {
     const location = useLocation();
     const receiver = location.state;
     const dispatch = useDispatch();
-    const { currentUser } = useSelector((state) => state.user)
+    let { currentUser } = useSelector(state => state.user)
+    const { chats } = useSelector(state => state.chat)
+    currentUser = JSON.parse(localStorage.getItem("user") || "{}");
+    const [message, setMessage] = useState("");
     useEffect(() => {
         dispatch(getuser())
+        dispatch(readMessage({ sender: currentUser.email, receiver: receiver.email }))
     }, [])
 
 
@@ -20,11 +24,18 @@ export default function ChatPage() {
             <div className="chat-main">
                 <div className="chat-veiw">
                     <h1>{receiver.email}</h1>
+                    <div>
+                        {
+                            chats.map((chat, index) => <p key={index}>{chat.message}</p>)
+                        }
+                    </div>
                 </div>
                 <div className="chat-input">
-                    <input type="text" placeholder='write the chat..' />
+                    <input type="text" placeholder='write the chat..' value={message} onChange={(e) => setMessage(e.target.value)} />
                     <button onClick={() => {
-                        dispatch(sendMessage({ sender: currentUser.email, receiver: receiver.email }))
+                        dispatch(sendMessage({ sender: currentUser.email, receiver: receiver.email, message: message }))
+                        dispatch(readMessage({ sender: currentUser.email, receiver: receiver.email }))
+                        setMessage("");
                     }}>send</button>
                 </div>
             </div>
--- chat-app/src/slices/chatSlice.js
+++ chat-app/src/slices/chatSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { db } from '../firebase.js'
-import { doc, setDoc } from 'firebase/firestore';
+import { collection, doc, getDocs, setDoc } from 'firebase/firestore';
 
 const initialState = {
     chats: [],
@@ -8,12 +8,34 @@ const initialState = {
     error: null
 }
 
-export const sendMessage = createAsyncThunk("chat/post", async ({ sender, receiver }) => {
-    const date = new Date();
-    const chatId = (date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()).toString();
-    const docId = sender + "_" + receiver;
+export const readMessage = createAsyncThunk("chat/get", async ({ sender, receiver }) => {
+    try {
+        const users = [sender, receiver];
+        users.sort();
+        const docId = users[0] + "_" + users[1];
+        const snapRef = collection(db, "chatbox", docId, "chats");
+
+        const Snapshot = await getDocs(snapRef);
+
+        const chats = Snapshot.docs.map(doc => doc.data());
+
+        return chats;
+    } catch (err) {
+        console.log("READ ERROR:", err.message);
+        throw err;
+    }
+});
+
+export const sendMessage = createAsyncThunk("chat/post", async ({ sender, receiver, message }) => {
+
+    const chatId = Date.now().toLocaleString();
+    const users = [sender, receiver];
+    users.sort();
+    const docId = users[0] + "_" + users[1];
     await setDoc(doc(db, "chatbox", docId, "chats", chatId), {
-        message: "hello! this is an message"
+        message: message,
+        sender: sender,
+        receiver: receiver
     })
 })
 
@@ -29,7 +51,16 @@ const chatSlice = createSlice({
         }).addCase(sendMessage.rejected, (state) => {
             state.error = "message is not able to sent";
             alert("message is not able to sent");
-        })
+        }).addCase(readMessage.pending, (state) => {
+            state.isLoading = true;
+        }).addCase(readMessage.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.chats = action.payload;
+        }).addCase(readMessage.rejected, (state) => {
+            state.isLoading = false;
+            state.error = "massage is not readable";
+            console.log("message is not readable");
+        });
     }
 })
 
